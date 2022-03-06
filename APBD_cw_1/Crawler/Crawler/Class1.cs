27aadedda95aca57b8d858c4021e13dd5d5056df
Kamil Bieleń <s20684@pjwstk.edu.pl>
2022-03-06 12:40:56 +0100

    Drugi commit

---
+++ APBD_cw_1/Crawler/Crawler/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Class1 : Interface1
+    {
+        private int _myProperty;
+        public int MyProperty { 
+            //get { return _myProperty * 1.23; }
+            set { _myProperty = value; }
+        }
+        public int Zmienna { get; set; }
+
+        public void Metoda() {
+            try { }
+            catch { }
+        }
+    }
+}
--- APBD_cw_1/Crawler/Crawler/Program.cs
+++ APBD_cw_1/Crawler/Crawler/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
     class Program
     {
-        static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var websiteUrl = args[0];
+            Console.WriteLine(websiteUrl);
+
+            //HttpClient httpClient = new HttpClient();
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(websiteUrl);
+            //Console.WriteLine(response);
+            var content = await response.Content.ReadAsStringAsync();
+            //Console.WriteLine(content);
+            //var regex = new Regex("\\w{5}@pja.edu.pl");
+            //var regex = new Regex("\\w{5}@\\w{3}.\\w{3}.\\w{2}");
+            //var regex = new Regex("\\w{5}@.*");
+            var regex = new Regex("([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+)");
+            //var a = $"Content {content}";
+            //var b = @"\.-]";
+
+            var matchCollection = regex.Matches(content);
+            foreach (var item in matchCollection) {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
